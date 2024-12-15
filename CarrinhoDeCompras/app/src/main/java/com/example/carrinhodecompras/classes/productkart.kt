f2a433b52257290549ba138464a811518e133def
Goncalo04Costa <a26024@alunos.ipca.pt>
2024-12-15 15:30:25 +0000

    Noticias
    Noticias

--- CarrinhoDeCompras/app/src/main/java/com/example/carrinhodecompras/classes/productkart.kt
+++
@@ -1,6 +0,0 @@
-package com.example.carrinhodecompras.Classes
-
-class ProdutoCarrinho (
-    val produto: Produto?=Produto("",0,"",""),
-    var quantidade: Int?=1
-)
--- Noticias/app/build.gradle.kts
+++ Noticias/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.noticias"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.noticias"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
